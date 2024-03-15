c4430dc07f0e51a4894e3ba7f60b1d1778481c2f
Guruprasad Subhash Tandlekar <67339327+guruu27@users.noreply.github.com>
2024-03-15 18:06:59 -0400

    Updates made

--- predict.py
+++ predict.py
@@ -22,17 +22,15 @@ def show_predict_page():
 
     st.write("""### We need some information to predict the salary""")
 
-    countries = (
-        "United States",
-        "Canada"
-      
+    country = (
+        "United States of America",
+        "Canada" 
     )
 
     age = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
+        '35-44 years old', '25-34 years old', '55-64 years old',
+       '18-24 years old', '45-54 years old', '65 years or older',
+       'Under 18 years old', 'Prefer not to say'
     )
     
     education = (
@@ -42,33 +40,38 @@ def show_predict_page():
         "Post grad",
     )
     
-    education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
+    devtype = (
+        'Full-Stack/Backend/Frontend Dev',
+        'Data Specialist(AI/ML/DBMS)',
+        'Managers/Team leads',
+        'Other professionals',
     )
     
-    education = (
-        "Less than a Bachelors",
-        "Bachelor’s degree",
-        "Master’s degree",
-        "Post grad",
+    industry = (
+        'IT industry professional',
+        'Financial professional',
+        'Different Type of Sector Professional',
+        'Other',
+        
     )
 
-    country = st.selectbox("Country", countries)
+    country = st.selectbox("Country", country)
     education = st.selectbox("Education Level", education)
-    education = st.selectbox("Education Level", education)
-    education = st.selectbox("Education Level", education)
-    education = st.selectbox("Education Level", education)
-    expericence = st.slider("Years of Experience", 0, 50, 3)
+    devtype = st.selectbox("Developer Type", devtype)
+    industry = st.selectbox("Industry you are working in", industry)
+    age = st.selectbox("Age group", age)
+    experience = st.slider("Years of Experience", 0, 50, 3)
 
     ok = st.button("Calculate Salary")
     if ok:
-        X = np.array([[country, education, expericence ]])
-        X[:, 0] = le_country.transform(X[:,0])
+        X = np.array([[age, education,devtype, country, experience, industry]])
+        X[:, 0] = le_age.transform(X[:,0])
         X[:, 1] = le_education.transform(X[:,1])
-        X = X.astype(float)
+        X[:, 2] = le_dev.transform(X[:,2])
+        X[:, 3] = le_country.transform(X[:,3])
+        X[:, 5] = le_ind.transform(X[:,5])
 
+        X = X.astype(float)
+        
         salary = regressor.predict(X)
         st.subheader(f"The estimated salary is ${salary[0]:.2f}")
